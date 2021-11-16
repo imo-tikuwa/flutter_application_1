aa18a0dc17754cce0420747e56ce0f4ee4654c66
imo-tikuwa <imotikuwabathclean@gmail.com>
2021-11-16 21:11:56 +0900

    ステートレスとステートフルの使い分けが分からないので読みやすいステートレスウィジェットに書き換え。。

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,44 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Welcome to Flutter',
-      home: RandomWords()
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) {
-            return const Divider(
-              color: Colors.blue
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+        ),
+        body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (context, i) {
+            if (i.isOdd) {
+              return const Divider(
+                color: Colors.blue
+              );
+            }
+
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+
+            return ListTile(
+              title: Text(
+                index.toString() + ' ' + _suggestions[index].asPascalCase,
+                style: _biggerFont,
+              ),
             );
           }
-
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-
-          return ListTile(
-            title: Text(
-              index.toString() + ' ' + _suggestions[index].asPascalCase,
-              style: _biggerFont,
-            ),
-          );
-        }
+        ),
       ),
     );
   }
